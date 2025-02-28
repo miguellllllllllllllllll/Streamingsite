b41e2d78aa4473472b7b4c88916f24b282840164
TAASOJOA <miguel.sousaribeiro@swisscom.com>
2025-02-28 18:25:07 +0100

    tried to get imdb to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import MovieCast from "./Moviecast";
 import Navbar from "./Navbar";
 import Searchbar from "./Searchbar";
 import Sneakpeek from "./Sneakpeek";
@@ -8,6 +9,14 @@ function App() {
     <>
       <Navbar />
       <Searchbar />
+      <MovieCast />
+      <iframe
+        src="https://vidsrc.me/embed/movie?imdb=tt0345950"
+        style={{ width: "400px", height: "200px" }} // <-- Doppelte geschweifte Klammern!
+        frameBorder="0" // <-- camelCase für JSX
+        referrerPolicy="origin" // <-- camelCase für JSX
+        allowFullScreen // <-- kein `=`
+      ></iframe>
       <Sneakpeek />
     </>
   );
